b0fcc143c9eee55cbeb16360be1138efb2d266bc
Dmitriy Tsvettsikh <dmitrycvet@gmail.com>
2021-02-25 03:56:39 +0500

    set and get mtu

--- lib/dtls.c
+++ lib/dtls.c
@@ -17,6 +17,7 @@
 #include <node_api.h>
 #include <uv.h>
 #include <gnutls/gnutls.h>
+#include <gnutls/dtls.h>
 #include <stdlib.h>
 #include <assert.h>
 
@@ -38,6 +39,8 @@ static const napi_type_tag dtls_session_type_tag = {
 static void dtls_cleanup_hook(void*);
 static napi_value dtls_create_session(napi_env env, napi_callback_info cb);
 static void dtls_close_session(napi_env env, void* handle, void*);
+static napi_value dtls_set_mtu(napi_env env, napi_callback_info cb);
+static napi_value dtls_get_mtu(napi_env env, napi_callback_info cb);
 
 static dtls_session_t* dtls_open_handle() {
   return (dtls_session_t*) malloc(sizeof(dtls_session_t));
@@ -52,15 +55,23 @@ static void dtls_close_handle(dtls_session_t* dtls) {
 }
 
 NAPI_MODULE_INIT() {
-  napi_value gnutls_version, create_session;
+  napi_value gnutls_version, create_session, set_mtu, get_mtu;
 
   CALL(gnutls_global_init());
 
   NAPI_CALL(napi_add_env_cleanup_hook(env, dtls_cleanup_hook, NULL));
+
   NAPI_CALL(napi_create_string_utf8(env, GNUTLS_VERSION, NAPI_AUTO_LENGTH, &gnutls_version));
   NAPI_CALL(napi_set_named_property(env, exports, "gnutls_version", gnutls_version));
+
   NAPI_CALL(napi_create_function(env, NULL, 0, dtls_create_session, NULL, &create_session));
   NAPI_CALL(napi_set_named_property(env, exports, "create_session", create_session));
+
+  NAPI_CALL(napi_create_function(env, NULL, 0, dtls_set_mtu, NULL, &set_mtu));
+  NAPI_CALL(napi_set_named_property(env, exports, "set_mtu", set_mtu));
+
+  NAPI_CALL(napi_create_function(env, NULL, 0, dtls_get_mtu, NULL, &get_mtu));
+  NAPI_CALL(napi_set_named_property(env, exports, "get_mtu", get_mtu));
 
   return exports;
 }
@@ -99,11 +110,63 @@ static napi_value dtls_create_session(napi_env env, napi_callback_info cb) {
 
   CALL(gnutls_init(&dtls->session, (unsigned int)flags));
   CALL(gnutls_certificate_allocate_credentials(&dtls->credentials));
-
+  CALL(gnutls_set_default_priority(dtls->session));
 
   NAPI_CALL(napi_create_object(env, &result));
   NAPI_CALL(napi_type_tag_object(env, result, &dtls_session_type_tag));
   NAPI_CALL(napi_wrap(env, result, dtls, &dtls_close_session, NULL, NULL));
+
+  return result;
+}
+
+static napi_value dtls_set_mtu(napi_env env, napi_callback_info cb) {
+  int32_t mtu = 0;
+  size_t argc = 2;
+  napi_value argv[2], result;
+  bool is_dtls_session;
+  dtls_session_t* dtls;
+
+  NAPI_CALL(napi_get_cb_info(env, cb, &argc, argv, NULL, NULL));
+  if (argc < 2) {
+    napi_throw_error(env, NULL, "Missing arguments");
+  }
+
+  NAPI_CALL(napi_check_object_type_tag(env, argv[0], &dtls_session_type_tag, &is_dtls_session));
+  if (!is_dtls_session) {
+    napi_throw_type_error(env, NULL, "Invalid session handle");
+  }
+
+  NAPI_CALL(napi_get_value_int32(env, argv[1], &mtu));
+  if (mtu <= 0) {
+    napi_throw_error(env, NULL, "Invalid mtu value");
+  }
+
+  NAPI_CALL(napi_unwrap(env, argv[0], (void**)&dtls));
+  gnutls_dtls_set_mtu(dtls->session, (unsigned int)mtu);
+
+  return result;
+}
+
+static napi_value dtls_get_mtu(napi_env env, napi_callback_info cb) {
+  unsigned int mtu = 0;
+  size_t argc = 1;
+  napi_value argv[1], result;
+  bool is_dtls_session;
+  dtls_session_t* dtls;
+
+  NAPI_CALL(napi_get_cb_info(env, cb, &argc, argv, NULL, NULL));
+  if (argc == 0) {
+    napi_throw_error(env, NULL, "Missing arguments");
+  }
+
+  NAPI_CALL(napi_check_object_type_tag(env, argv[0], &dtls_session_type_tag, &is_dtls_session));
+  if (!is_dtls_session) {
+    napi_throw_type_error(env, NULL, "Invalid session handle");
+  }
+
+  NAPI_CALL(napi_unwrap(env, argv[0], (void**)&dtls));
+  mtu = gnutls_dtls_get_mtu(dtls->session);
+  NAPI_CALL(napi_create_uint32(env, mtu, &result));
 
   return result;
 }
--- test/unit/native.js
+++ test/unit/native.js
@@ -20,3 +20,32 @@ test('should check session flags', () => {
   expect(() => dtls.create_session()).toThrowError('Expected GnuTLS session init flags');
   expect(() => dtls.create_session(-1)).toThrowError('Invalid GnuTLS session init flags');
 });
+
+describe('test mtu', () => {
+  let session;
+  const mtu = 1420;
+
+  beforeAll(() => {
+    session = dtls.create_session(GNUTLS_CLIENT | GNUTLS_DATAGRAM);
+    expect(session).toEqual({});
+  });
+
+  it('should set mtu', () => {
+    expect(dtls.set_mtu(session, mtu)).not.toBeNull();
+  });
+
+  it('should get mtu', () => {
+    expect(dtls.get_mtu(session)).toEqual(mtu);
+  });
+
+  it('should check arguments on set mtu', () => {
+    expect(() => dtls.set_mtu()).toThrowError('Missing arguments');
+    expect(() => dtls.set_mtu({}, mtu)).toThrowError('Invalid session handle');
+    expect(() => dtls.set_mtu(session, -1)).toThrowError('Invalid mtu value');
+  });
+
+  it('should check arguments on get mtu', () => {
+    expect(() => dtls.get_mtu()).toThrowError('Missing arguments');
+    expect(() => dtls.get_mtu({})).toThrowError('Invalid session handle');
+  });
+});
